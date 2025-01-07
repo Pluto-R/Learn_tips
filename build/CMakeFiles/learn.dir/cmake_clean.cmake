7eb4425575e242e57d665958eb85e4e50e4045ba
jqz <jqz0316@gmail.com>
2025-01-07 21:52:21 +0800

    multithreading basic use

---
+++ build/CMakeFiles/learn.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learn.dir/src/main.cpp.o"
+  "CMakeFiles/learn.dir/src/main.cpp.o.d"
+  "CMakeFiles/learn.dir/src/multithread.cpp.o"
+  "CMakeFiles/learn.dir/src/multithread.cpp.o.d"
+  "learn"
+  "learn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/learn.dir/src/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/learn.dir/src/main.cpp.o: \
+ /home/jqz/Desktop/Learn_tips/src/main.cpp /usr/include/stdc-predef.h
